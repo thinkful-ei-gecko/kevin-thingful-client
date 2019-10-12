668c92ff1d39fdc5e51147f00087ae5437b349b5
Kevin Wei <chenghaowei@gmail.com>
2019-10-12 11:30:55 -0400

    add jwt auth

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -5,21 +5,21 @@ const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
       headers: {
-        aUtHoRiZaTioN: `basic ${TokenService.getAuthToken()}`,
+        aUtHoRiZaTioN: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) => (!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()));
   },
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
-        aUtHoRiZaTioN: `basic ${TokenService.getAuthToken()}`,
+        aUtHoRiZaTioN: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) => (!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()));
   },
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
-        aUtHoRiZaTioN: `basic ${TokenService.getAuthToken()}`,
+        aUtHoRiZaTioN: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) => (!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()));
   },
@@ -28,7 +28,7 @@ const ThingApiService = {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-        aUtHoRiZaTioN: `basic ${TokenService.getAuthToken()}`,
+        aUtHoRiZaTioN: `bearer ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         thing_id: thingId,
